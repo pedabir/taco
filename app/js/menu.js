bbda2d89a9ed9e55fd238a3abe600bd356848c68
pedabir <i.makhonkov@hotmail.com>
2018-06-19 00:46:28 +0300

    add mobile menu + soft scroll

--- app/js/menu.js
+++ app/js/menu.js
@@ -1,14 +1,13 @@
 jQuery(document).ready(function($) {
     // Клик на элемент по которому и будет совершаться
     // действие открытие и закрытие sidebar
-    $('.hamburger').click(function(e) {
+    $('.icon-menu').click(function(e) {
       // Класс sidebar
       var sidebar = $('.menu');
       // Ширина sidebar
       var sidebarWidth = $('.menu').width();
       // Добавляем класс open
       $('.wrapper').addClass('open');
-
       // Проверяем, если в sidebar свойство left не равна ширины его то выполняються
       // следующие действия
       if (sidebar.css('left') != sidebarWidth) {
@@ -30,37 +29,3 @@ jQuery(document).ready(function($) {
       e.preventDefault();
     });
   });
-
-// jQuery(document).ready(function($) {
-// 			$('.hamburger').click(function(e) {
-//         sidebar.toggleClass('open');
-//       });
-//     });
-// 		var sidebar = $('.menu');
-// 				// // Ширина sidebar
-// 				var sidebarWidth = $('.menu').width();
-// 				// Добавляем класс active
-// 				$('.header, .top-banner, .slider-counteiner').addClass('open');
-//
-// 				// Проверяем, если в sidebar свойство left не равна ширины его то выполняються
-// 				// следующие действия
-// 				// if (sidebar.css('left') != sidebarWidth) {
-// 					// Добавляем ему класс active
-// 					// sidebar.addClass('open');
-//
-// 					var firstClick = true;
-// 					// Скрипт который будет закрывать sidebar при клике на любое место
-// 					$(document).bind('click.myEvent', function(e) {
-// 						if (!firstClick && $(e.target).closest('.menu').length == 0) {
-// 							// Удаляем класс active
-// 							sidebar.removeClass('open');
-// 							// Удаляем класс active
-// 							// $('.header, .content').removeClass('open');
-// 							$(document).unbind('click.myEvent');
-// 						}
-// 						firstClick = false;
-// 					});
-
-				// e.preventDefault();
-		// 	});
-		// });
